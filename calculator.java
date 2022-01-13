3dbda5e9e9d3aab1de5b6b9f610f547fa59dbb10
[thabangmotseta] <[thabangabelmotseta@gmail.com]>
2022-01-13 10:50:00 +0200

    calculator

--- calculator.java
+++ calculator.java
@@ -9,13 +9,14 @@ class Main {
     // create an object of Scanner class
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -");
-    operator = input.next().charAt(0);
 
     // ask users to enter numbers
     System.out.println("Enter first number");
     number1 = input.nextInt();
+
+ // ask users to enter operator
+    System.out.println("Choose an operator: +, -");
+    operator = input.next().charAt(0);
 
     System.out.println("Enter second number");
     number2 = input.nextInt();
@@ -33,8 +34,6 @@ class Main {
         result = number1 - number2;
         System.out.println(number1 + " - " + number2 + " = " + result);
         break;
-
-    
 
       default:
         System.out.println("Invalid operator!");
